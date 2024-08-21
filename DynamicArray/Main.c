9f2eec31d746edf53ebcb1d7aa9d9b18a970b34d
ASBackend <Arturo.Serpico@gmail.com>
2024-08-21 13:04:28 +0200

    Update Main.c

--- DynamicArray/Main.c
+++ DynamicArray/Main.c
@@ -10,38 +10,40 @@ int main() {
 	
 	int buffer[4] = { 1, 2, 3, 4 };
 
-	dynamicArrayAssignAll(int, test, buffer);
+	dynamicArrayAssignAll(int, &test, buffer);
 
 	printf("\n\ndynamicArrayAssignAll:\n\nsize of the array: %i\n", test.lenght);
 
 	for (int i = 0; i < test.lenght; i++)
-		printf("%i: %i\n", i, dynamicArrayGet(int, test, i));
+		printf("%i: %i\n", i, dynamicArrayGet(int, &test, i));
 
-	dynamicArrayFill(int, test, 5);
+	dynamicArrayFill(int, &test, 5);
 
 	printf("\n\ndynamicArrayFill:\n\nsize of the array: %i\n", test.lenght);
 
 	for (int i = 0; i < test.lenght; i++)
-		printf("%i: %i\n", i, dynamicArrayGet(int, test, i));
+		printf("%i: %i\n", i, dynamicArrayGet(int, &test, i));
 
-	dynamicArrayPushBack(int, test, 6);
+	dynamicArrayPushBack(int, &test, 6);
 
 	printf("\n\ndynamicArrayPushBack:\n\nsize of the array: %i\n", test.lenght);
 
 	for(int i = 0; i < test.lenght; i++)
-		printf("%i: %i\n", i, dynamicArrayGet(int, test, i));
+		printf("%i: %i\n", i, dynamicArrayGet(int, &test, i));
 
-	dynamicArrayPopBack(int, test);
+	dynamicArrayPopBack(int, &test);
 
 	printf("\n\ndynamicArrayPopBack:\n\nsize of the array: %i\n", test.lenght);
 
 	for (int i = 0; i < test.lenght; i++)
-		printf("%i: %i\n", i, dynamicArrayGet(int, test, i));
+		printf("%i: %i\n", i, dynamicArrayGet(int, &test, i));
 
-	dynamicArraySet(int, test, 3, 4);
+	dynamicArraySet(int, &test, 3, 4);
 
 	printf("\n\ndynamicArraySet:\n\nsize of the array: %i\n", test.lenght);
 
 	for (int i = 0; i < test.lenght; i++)
-		printf("%i: %i\n", i, dynamicArrayGet(int, test, i));
+		printf("%i: %i\n", i, dynamicArrayGet(int, &test, i));
+
+	dynamicArrayFree(int, &test);
 }
